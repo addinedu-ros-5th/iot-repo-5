930c89532eed4aac925e62422b9e67d00780ba4e
JYKim6780 <junyeongkim6780@gmail.com>
2024-04-19 21:30:43 +0900

    econstructed databases and created columns for selection GUI. Also organised the codesby creating functions.

--- GUI/smart_farmer.py
+++ GUI/smart_farmer.py
@@ -1,4 +1,5 @@
 import sys
+import mysql.connector
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
@@ -11,6 +12,46 @@ class WindowClass(QMainWindow, from_class) :
         self.setupUi(self)
 
         self.setWindowTitle("Smart Farmer")
+        
+        # Set Default Columns 
+        self.cur,  self.remote = self.getCursor()
+        names = self.getColNames()
+
+        for each in names:
+            print(each[0])
+            column_cnt = self.tableWidget.columnCount() # count total col <- index. 
+            self.tableWidget.insertColumn(column_cnt)
+            self.tableWidget.setHorizontalHeaderItem(column_cnt, QTableWidgetItem(each[0]))
+
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+    def getCursor(self):
+        remote = mysql.connector.connect(
+        host = "smart-farming-database.cb4y0k88ysov.ap-northeast-2.rds.amazonaws.com",
+        #port = '3306',
+        user = "admin",
+        password = "Louis66411!",
+        database = "smart_farming"
+        )   
+        cur = remote.cursor(buffered=True)
+        return [cur, remote]
+
+    
+    def getColTotal(self):
+        self.cur.execute("select COUNT(*) FROM information_schema.COLUMNS WHERE table_schema = 'smart_farming' AND table_name = 'plant';")
+        result = self.cur.fetchall()
+        totalCol = result[0][0] # [(6,),]
+
+        return totalCol
+
+    def getColNames(self):
+        self.cur.execute("select COLUMN_NAME FROM information_schema.COLUMNS WHERE table_schema = 'smart_farming' AND table_name = 'plant';")
+        col_names = self.cur.fetchall() # ex) [('온도',), ('습도')]
+        return col_names
+
+    def endConnection(self):
+        self.remote.end()
+        
   
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -18,3 +59,5 @@ if __name__ == "__main__":
     myWindows.show()
 
     sys.exit(app.exec_())
+
+    cur.end()
